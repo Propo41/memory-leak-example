b1d4491f7725784630978a893b88d12bf793f2af
Propo41 <aliahnaf327@gmail.com>
2024-12-04 09:41:57 +0600

    updated pageOne code

--- src/views/PageOne.vue
+++ src/views/PageOne.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
+const poems: any[] = []
+const count = ref(0)
 
 const onBackClick = () => {
   router.back()
 }
 
-const callMe = () => {
+const doSomething = () => {
   const largeArray = new Array(1000000).fill('Humpty dumpty')
+  poems.push(largeArray)
   console.log(largeArray)
+  count.value++
 }
 </script>
 
@@ -17,6 +22,8 @@ const callMe = () => {
   <h1>Page 1</h1>
   <div style="display: flex; flex-direction: column">
     <button @click="onBackClick">Back</button>
-    <button style="color: red" @click="() => callMe()">Call me</button>
+    <button style="color: red" @click="() => doSomething()">Do something</button>
+
+    <div>Do something count: {{ count }}</div>
   </div>
 </template>
